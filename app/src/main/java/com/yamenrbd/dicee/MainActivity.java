beae84aa62303bd76bea3f0f4b188bb20f6e2da0
yamen_rbd <yamenrbd@gmail.com>
2017-11-09 18:50:04 +0300

    dice new change

--- app/src/main/java/com/yamenrbd/dicee/MainActivity.java
+++ app/src/main/java/com/yamenrbd/dicee/MainActivity.java
@@ -17,16 +17,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button rollButton;
         rollButton = (Button) findViewById(R.id.rollButton);
-        ImageView leftDice = (ImageView) findViewById(R.id.image_diceLeft);
-        ImageView rightDice = (ImageView) findViewById(R.id.image_diceright);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_diceLeft);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_diceright);
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random randomNumberGenerator = new Random();
-                int number = randomNumberGenerator.nextInt(6);
-                Log.d("Dicee","the randome number is "+ number);
-                //this is test on Git
+                int number1 = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee","the randome number is "+ number1);
+
+                leftDice.setImageResource(diceArray[number1]);
+                number1 = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number1]);
+
             }
         });
 
